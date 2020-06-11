dc719e1e6bc6860dff0745015cdeb746d37e6195
JpabloMF <juanpablo45671@gmail.com>
2020-06-11 11:49:24 -0500

    framework and QUIT event

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Slither')
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+    gameDisplay.fill(white)
+    pygame.display.update() # update our display when we apply all the changes
+
+pygame.quit()
+quit()
